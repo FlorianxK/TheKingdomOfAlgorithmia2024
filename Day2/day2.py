498e318b7002728942430cb4e92e2796a44fe812
FlorianxK <Florian.Koehler@jokonet.de>
2026-03-03 21:22:56 +0100

    day2 finished

--- Day2/day2.py
+++ Day2/day2.py
@@ -53,21 +53,108 @@ def dayTwo2():
                     start += len(w)
 
                 for p in pos:
-                    for i in range(len(revRune)-1,len(revRune)-1-len(w),-1):
+                    originalStart = len(rune)-p-len(w)
+                    for i in range(originalStart,originalStart+len(w)):
                         bits[i] = 1
 
             lineCount += sum(bits)
-        print(lineCount)
         res += lineCount
 
     return res
 
 def dayTwo3():
-    pass
+    arr = []
+    #read
+    with open("Day2/2_3.txt") as file:
+        words = file.readline().strip().split(':')[1].split(',')
+        next(file)
+        for line in file:
+            arr.append(line.strip())
+    m,n = len(arr),len(arr[0])
+    bits = [[0]*n for _ in range(m)]
+
+    #l to r
+    for i,row in enumerate(arr):
+        row = row*2
+        for w in words:
+            pos = []
+            start = 0
+            while True:
+                start = row.find(w,start)
+                if start == -1:
+                    break
+                pos.append(start)
+                start += len(w)
+            
+            for p in pos:
+                if p < n:
+                    for k in range(p,p+len(w)):
+                        bits[i][k%n] = 1
+    
+    #r to l
+    for i,row in enumerate(arr):
+        revRow = row[::-1]*2
+        for w in words:
+            pos = []
+            start = 0
+            while True:
+                start = revRow.find(w,start)
+                if start == -1:
+                    break
+                pos.append(start)
+                start += len(w)
+
+            for p in pos:
+                if p < n:
+                    originalStart = len(revRow)-p-len(w)
+                    for k in range(originalStart,originalStart+len(w)):
+                        bits[i][k%n] = 1
+    
+    #t to b
+    for j,col in enumerate(zip(*arr)):
+        col = ''.join(col)
+        for w in words:
+            pos = []
+            start = 0
+            while True:
+                start = col.find(w,start)
+                if start == -1:
+                    break
+                pos.append(start)
+                start += len(w)
+            
+            for p in pos:
+                if p < m:
+                    for k in range(p,p+len(w)):
+                        bits[k%m][j] = 1
+
+    #b to t
+    for j,col in enumerate(zip(*arr)):
+        col = ''.join(col)
+        col = col[::-1]
+        for w in words:
+            pos = []
+            start = 0
+            while True:
+                start = col.find(w,start)
+                if start == -1:
+                    break
+                pos.append(start)
+                start += len(w)
+
+            for p in pos:
+                if p < m:
+                    originalStart = len(col)-p-len(w)
+                    for k in range(originalStart,originalStart+len(w)):
+                        bits[k%m][j] = 1
+    res = 0
+    for b in bits:
+        res += sum(b)
+    return res
 
 def main():
     print("Hallo")
-    #print(dayTwo(), "ist die Lösung von Teil 1")
+    print(dayTwo(), "ist die Lösung von Teil 1")
     print(dayTwo2(), "ist die Lösung von Teil 2")
     print(dayTwo3(), "ist die Lösung von Teil 3")
 
