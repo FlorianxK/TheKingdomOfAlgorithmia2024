129866efd0249748427c49fa0d17e686e716c73b
FlorianxK <Florian.Koehler@jokonet.de>
2026-04-16 19:06:16 +0200

    day16 finished

--- Day16/day16.py
+++ Day16/day16.py
@@ -1,4 +1,6 @@
 from collections import defaultdict
+from functools import cache
+from itertools import zip_longest
 from typing import *
 
 def daySixteen():
@@ -128,13 +130,58 @@ def daySixteen2():
     return res
     
 def daySixteen3():
-    pass
+    with open("Day16/16_3.txt") as file:
+        header,_,*remainder = file.read().splitlines()
+    
+    wheel_distances = [int(x) for x in header.split(",")]
+    wheels_transpose = []
+    for line in remainder:
+        row_symbols = []
+        for i in range(0,len(line),4):
+            symbol = line[i:i+3].strip()
+            row_symbols.append(symbol)
+        wheels_transpose.append(row_symbols)
+    
+    wheels = []
+    for wheel_symbols in zip_longest(*wheels_transpose, fillvalue=""):
+        filtered = [s for s in wheel_symbols if s]
+        processed = [w[::2] for w in filtered]
+        wheels.append(tuple(processed))
 
+    @cache
+    def maxmin_score(wheel_offset=0,pull_number=0,pulls_remaining=256):
+        symbols = []
+        for dist,wheel in zip(wheel_distances,wheels):
+            index = (pull_number * dist + wheel_offset) % len(wheel)
+            symbol = wheel[index]
+            symbols.append(symbol)
+        line = "".join(symbols)
+        if pull_number:
+            counts = Counter(line)
+            score = 0
+            for count in counts.values():
+                if count > 2:
+                    score += count-2
+        else:
+            score = 0
+        if pulls_remaining:
+            results = []
+            for i in (-1,0,1):
+                result = maxmin_score(wheel_offset+i,pull_number+1,pulls_remaining-1)
+                results.append(result)
+            maxs = [r[0] for r in results]
+            mins = [r[1] for r in results]
+            return score+max(maxs), score+min(mins)
+        return score,score
+
+    maxRes,minRes = maxmin_score()
+    return f"{maxRes} {minRes}"
+    
 def main():
     print("Hallo")
     print(daySixteen(), "ist die Lösung von Teil 1")
     print(daySixteen2(), "ist die Lösung von Teil 2")
-    #print(daySixteen3(), "ist die Lösung von Teil 3")
+    print(daySixteen3(), "ist die Lösung von Teil 3")
 
 if __name__=="__main__":
     main()
